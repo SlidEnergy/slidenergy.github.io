3ca961e48af5f3cc52bd3df8e98d464f1282f235
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-29 14:02:35 +0000

    Deploy slidenergy/slidenergy.github.io to github.com/slidenergy/slidenergy.github.io.git:master

--- src/app/projects/api/project.ts
+++
@@ -1,9 +0,0 @@
-export interface Project {
-    titleRu: string;
-    titleEn: string;
-    tags: string;
-    images: any[];
-    descriptionRu: string;
-    descriptionEn: string;
-    category: string;
-}
--- src/app/shared/noindex.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NoindexPipe } from './noindex.pipe';
-
-describe('NoindexPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NoindexPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
